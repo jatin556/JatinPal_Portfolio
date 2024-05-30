e3ad157aa4130553585de5ac07a07027e5464d27
jatin556 <43544808+jatin556@users.noreply.github.com>
2024-05-30 19:30:50 +0100

    cc app

--- force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
+++ force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
+import { countryCodeList } from 'c/countryCodeList';
+import currencyConverterAssets from '@salesforce/resourceUrl/currencyConverterAssets';
 
-export default class CurrencyConverterApp extends LightningElement {}
+export default class CurrencyConverterApp extends LightningElement {
+    currencyImage = currencyConverterAssets + '/currencyConverterAssets/currency.svg';
+    countryList = countryCodeList;
+    countryFrom = 'USD';
+    countryTo = 'GBP';
+    handleChange(event) {
+        const {name, value} = event.target;
+        console.log('name: ', name);
+        console.log('value: ', value);
+        this[name] = value;
+    }
+}
