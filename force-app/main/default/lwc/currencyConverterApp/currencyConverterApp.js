2cfe77fd9c81d199ec3afe724bcbca413d132036
Jatin Pal <jatinpal556@gmail.com>
2024-05-31 16:01:12 +0530

    final cc app

--- force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
+++ force-app/main/default/lwc/currencyConverterApp/currencyConverterApp.js
@@ -7,10 +7,40 @@ export default class CurrencyConverterApp extends LightningElement {
     countryList = countryCodeList;
     countryFrom = 'USD';
     countryTo = 'GBP';
+    amount = '';
+    result;
+    error;
     handleChange(event) {
         const {name, value} = event.target;
         console.log('name: ', name);
         console.log('value: ', value);
         this[name] = value;
+        this.result = '';
+        this.error = '';
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+        this.convert();
+    }
+
+    async convert() {
+        const API_KEY = '2a2dc7275a792e89440470f6';
+        const API_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${this.countryFrom}/${this.countryTo}`;
+
+        try {
+            const data = await fetch(API_URL);
+            const jsonData = await data.json();
+            console.log(jsonData);
+            this.result = (Number(this.amount) * jsonData.conversion_rate).toFixed(2);
+            console.log('Amount: ', typeof this.amount);
+            console.log('Amount: ', typeof Number(this.amount));
+            console.log('Conversion Rate: ', jsonData.conversion_rate);
+            console.log('Conversion Rate: ', typeof jsonData.conversion_rate);
+            console.log(this.result);
+        } catch (error) {
+            console.log(error);
+            this.error = 'Some error has occured, please try again later!';
+        }
     }
 }
